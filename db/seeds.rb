ca06f2e5c2ca70bcc11055ebbd5f75bbd07443d4
Etienne Testard <etitestard@gmail.Com>
2019-11-15 15:37:08 +0100

    Exo important CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+
+
+Cocktail.create(name: "Annioutise")
+Cocktail.create(name: "Le Meldois")
+
+Dose.create(description: "Lorem Ipsum", name: "Lorem Ipsum")
+
